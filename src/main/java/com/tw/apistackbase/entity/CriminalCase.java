bf49d404cf6f1dbfde10a910a35b34fca3f94145
zzw <kenzzw@163.com>
2019-07-17 20:52:02 +0800

    ADD: Story 1 AC1 , add class CriminalCase

---
+++ src/main/java/com/tw/apistackbase/entity/CriminalCase.java
@@ -0,0 +1,45 @@
+package com.tw.apistackbase.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class CriminalCase {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid",strategy = "uuid")
+    private String id;
+    private Long time;
+    @Length(min = 0,max = 255)
+    @NotNull
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
